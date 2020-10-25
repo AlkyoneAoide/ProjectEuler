a3a902827f758474f070f08774e42c2abe0efca9
AlkyoneAoide <playinggameswithfriends@gmail.com>
2020-10-24 22:26:08 -0600

    new problems hooray

---
+++ 13.js
@@ -0,0 +1,33 @@
+(_=>{
+    let divisors = n=>{
+        let d = 0
+        let r = Math.ceil(Math.sqrt(n))
+        for (let i = 1; i <= r; i++) {
+            if (n%i == 0) {
+                d = d + 2
+            }
+        }
+        return d
+    }
+
+    let dmax = 0
+    let t = 0
+
+    for (let n = 1; n < 50000; n++) {
+        t = t + n
+
+        let d = divisors(t)
+        if (d > dmax) {
+            dmax = d
+            console.log("t:", t, "n:", n, "dmax:", dmax)
+
+        }
+
+        if (d > 500) {
+            console.log(t, "done")
+            return false
+        }
+    }
+    console.log("done")
+}
+)()
